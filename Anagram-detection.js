c60f0586673d784673d052d0b876bef60eb20160
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-11-04 12:04:57 -0800

    Update Anagram-detection.js

--- Anagram-detection.js
+++ Anagram-detection.js
@@ -14,3 +14,8 @@ Examples:
 
 
 // Solution
+
+let isAnagram = function(test, original) {
+  if(test.length !== original.length) { return false }
+  return [...test.toLowerCase()].sort().join("") === [...original.toLowerCase()].sort().join("")
+};
